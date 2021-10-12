1c0b61e2514c195902a7deb4d3459c664ae01e42
seoyeon <haeshs99@naver.com>
2021-10-13 06:43:44 +0900

    delete remark is not needed

--- src/moon_lander/Window.java
+++ src/moon_lander/Window.java
@@ -15,34 +15,14 @@ public class Window extends JFrame{
         
     private Window()
     {
-        // Sets the title for this frame.
         this.setTitle("Moon Lander");
+        this.setSize(2000, 1500);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);        
         
-        // Sets size of the frame.
-        if(false) // Full screen mode
-        {
-            // Disables decorations for this frame.
-            this.setUndecorated(true);
-            // Puts the frame to full screen.
-            this.setExtendedState(this.MAXIMIZED_BOTH);
-        }
-        else // Window mode
-        {
-            // Size of the frame.
-            this.setSize(800, 600);
-            // Puts frame to center of the screen.
-            this.setLocationRelativeTo(null);
-            // So that frame cannot be resizable by the user.
-            this.setResizable(false);
-        }
-        
-        // Exit the application when user close frame.
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        this.setContentPane(new Framework());
-        
-        this.setVisible(true);
-        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
+        this.setContentPane(new Framework());        
+        this.setVisible(true);        
     }
 
 	public static void main(String[] args)

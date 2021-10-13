576e3b9bc09c54d7b047fff542b945ec3c5814d9
seoyeongo <haeshs99@naver.com>
2021-10-14 05:14:28 +0900

    add: upload files

---
+++ src/sprite/Shot.java
@@ -0,0 +1,27 @@
+package sprite;
+
+import javax.swing.ImageIcon;
+
+public class Shot extends Sprite  {
+	public Shot() {
+		super();
+	}
+	
+	
+	public Shot(int x, int y) {
+		initShot(x, y);
+	}
+	
+	private void initShot(int x, int y) {
+		
+		String shotImg = "./resources/images/sprite/shot.png";
+		ImageIcon img = new ImageIcon(shotImg);
+		setImage(img.getImage());
+		
+		int X_SPACE = 6;
+		setX(x + X_SPACE);
+		
+		int Y_SPACE = 1;
+		setY(y - Y_SPACE);
+	}
+}
